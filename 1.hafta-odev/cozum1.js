6c229eb15ef1bfc976aec01e300bbda65154fd8d
YusraSenimDede <50294199+YusraSenimDede@users.noreply.github.com>
2021-05-01 14:16:06 +0300

    first commit

---
+++ 1.hafta-odev/cozum1.js
@@ -0,0 +1,23 @@
+// görev 1
+$(gorev1).onClick(function(){
+    $("h2").parent(); 
+
+});
+
+// görev 2
+
+// görev 3
+
+// görev 4
+
+// görev 5
+
+// görev 6
+
+// görev 7
+
+// görev 8
+
+// görev 9
+
+// görev 10
